1ce246c418216d8f69ac3e9974cf9682b511c435
islacrur <islacrur@gmail.com>
2024-03-21 13:26:40 +0100

    Install Tailwind

---
+++ src/configuration/Router.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+
+const Router = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Home />} path='/' />
+                </Routes>
+            </BrowserRouter>
+        </>
+
+    )
+}
+
+export default Router
