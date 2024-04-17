423a9d5d528a9b5a817463c1cb425a08cf0e5e8e
mariamf1985 <mariamf1985@gmail.com>
2024-04-17 09:54:29 +0200

    add image navbar

---
+++ src/assets/components/Atoms/Banner_nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../../../images/Matchathon_navbar.png'
+
+const Banner_nav = () => {
+  return (
+<div>
+<img src={Banner} alt="Descripción de la imagen" />
+</div>
+  )
+}
+
+export default Banner_nav
--- src/assets/views/Home.jsx
+++ src/assets/views/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import FormCoders from '../components/FormCoders';
+import Banner_nav from '../components/Atoms/Banner_nav';
 
 const Home = () => {
   return (
     <>
-
+    <Banner_nav/>
     <FormCoders/>
     </>
   )
