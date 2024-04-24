6e9c034dd8f7e68cb86dcb9fb3cef0f742162442
sandovaljohana <sandovaljohana0207@gmail.com>
2024-04-24 13:44:08 +0200

    adding props to teamAll component

--- src/assets/components/Atoms/TeamCard.jsx
+++ src/assets/components/Atoms/TeamCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const TeamCard = ({ team_id, user_id, firstName, lastName, bootcamp, level }) => {
+const TeamCard = ({ team_id, user_id, firstName, lastName, bootcamp, backendTechLevel, frontendTechLevel, controlVersionStack }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto my-4">
       <div className="px-6 py-4">
@@ -9,7 +9,12 @@ const TeamCard = ({ team_id, user_id, firstName, lastName, bootcamp, level }) =>
           <span className="font-bold">ID de equipo:</span> {team_id}<br />
           <span className="font-bold">ID de usuario:</span> {user_id}<br />
           <span className="font-bold">Bootcamp:</span> {bootcamp}<br />
-          <span className="font-bold">Nivel:</span> {level}
+          <span className="font-bold">backendTechLevel:</span> {backendTechLevel}
+          <br />
+          <span className="font-bold">frontendTechLevel:</span> {frontendTechLevel}
+          <br />
+          <span className="font-bold">controlVersionStack:</span> {controlVersionStack}
+          <br />
         </p>
       </div>
     </div>
--- src/assets/components/TeamAll.jsx
+++ src/assets/components/TeamAll.jsx
@@ -8,7 +8,7 @@ const TeamAll = () => {
   useEffect(() => {
     const fetchTeams = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/api/teams'); // Ruta de la API para obtener equipos
+        const response = await axios.get('http://127.0.0.1:8000/api/teams'); 
         setTeams(response.data);
       } catch (error) {
         console.error('Error fetching teams:', error);
@@ -23,17 +23,19 @@ const TeamAll = () => {
       <h1 className="text-3xl font-semibold text-center my-8">Lista de Equipos</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {teams.map(team => (
-          <div key={team.id}> {/* Usamos el id único del equipo como key */}
-            <h2 className="text-2xl font-semibold mb-2">{team.team}</h2> {/* Mostrar el nombre del equipo */}
+          <div key={team.id}>
+            <h2 className="text-2xl font-semibold mb-2">{team.team}</h2>
             {team.users.map(user => (
               <TeamCard
-                key={user.id} // Usar el id único del usuario como key
-                team_id={team.id} // Pasar el id del equipo
-                user_id={user.id} // Pasar el id del usuario
-                firstName={user.name} // Nombre del usuario
-                lastName={user.lastname} // Apellido del usuario
-                bootcamp={user.bootcamp_id} // Acceder al bootcamp del usuario
-                level={user.pivot.level_id} // Nivel del usuario (asegúrate de obtenerlo correctamente)
+                key={user.id}
+                team_id={team.id}
+                user_id={user.id}
+                firstName={user.name}
+                lastName={user.lastname}
+                bootcamp={user.bootcamp_name}
+                backendTechLevel={user.backend_technologies?.[0]?.pivot?.level_id}
+                frontendTechLevel={user.frontend_technologies?.[0]?.pivot?.level_id}
+                controlVersionStack={user.control_versions?.[0]?.pivot?.level_id}
               />
             ))}
           </div>
@@ -44,5 +46,3 @@ const TeamAll = () => {
 };
 
 export default TeamAll;
-
-
